d42cacc40f6074772d6cfbb0dec7d4783f5a7fe8
hymndrazill <srihafiras@yahoo.fr>
2022-10-01 17:48:00 +0100

    api locked and loaded

---
+++ api/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("connected to mongo"))
+  .catch((err) => console.log(err));
+
+app.listen("5000", () => {
+  console.log("backend is running");
+});
